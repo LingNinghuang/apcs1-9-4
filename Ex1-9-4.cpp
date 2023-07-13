e007706f57e3644889a716dcb1e93917ef5ebe92
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-13 15:56:22 +0800

    Create Ex1-9-4.cpp

---
+++ Ex1-9-4.cpp
@@ -0,0 +1,12 @@
+//operators
+#include <stdio.h>
+int main(){
+    int x = 24, y = 11;
+    printf("%d\n", x == y);
+    printf("%d\n", x != y);
+    printf("%d\n", x > y);
+    printf("%d\n", x < y);
+    printf("%d\n", x >= y);
+    printf("%d", x <= y);
+    return 0;
+}
